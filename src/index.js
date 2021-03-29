fcd29ab2cca3cab3fb4e0e59b1ba833302e96c5c
Zahra <hizahrazadeh@gmail.com>
2021-03-29 19:44:15 -0400

    hash

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { run } from "./app/app";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import "./main.scss";
-
+console.log('hi')
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // devtool: false,
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
