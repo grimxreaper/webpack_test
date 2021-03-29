40d6693f9c3bd0d634c254eadf5767c335e2f41a
Zahra <hizahrazadeh@gmail.com>
2021-03-29 19:58:28 -0400

    html config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: "development",
     // devtool: false,
     entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
     output: {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
