761e2b80b25071ab03471cc55a01d7d2e0d621e4
Zahra <hizahrazadeh@gmail.com>
2021-03-30 19:50:18 -0400

    Contd. Debugging the img
    -tried different fixes that did not work incl. setting esModule to false
    -entering output to set public path to an empty string, etc.

--- webpack.common.js
+++ webpack.common.js
@@ -1,46 +1,45 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    // devtool: false,
-    entry: {
-        main: "./src/index.js",
-        vendor: "./src/vendor.js"
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
-    module: {
-        rules: [
-            {
-            test: /\.scss$/,
-            use: [
-                "style-loader", //3. inject styles into DOM
-                "css-loader", //2. turns css into commonjs
-                "sass-loader" //1. turns sass into css
-            ]
-            },
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(svg|png|jpeg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        esModule: false,
-                        name: "[name].[hash].[ext]",
-                        outputPath: "imgs",
-                        publicPath: "imgs"
-                        }
-                },
-                
-            }
-        ]
-    },
+  // devtool: false,
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. inject styles into DOM
+          "css-loader", //2. turns css into commonjs
+          "sass-loader", //1. turns sass into css
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpeg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false,
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs",
+            publicPath: "/",
+          },
+        },
+      },
+    ],
+  },
 
-    // output: { publicPath: ''}
-    
-}
+//   output: { publicPath: ''}
+};
