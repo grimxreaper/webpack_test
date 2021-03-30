17432eaf8e29788e6dd015431db1a3047d42d25c
Zahra <hizahrazadeh@gmail.com>
2021-03-30 16:35:59 -0400

    add prod and dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,12 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
 
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,11 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
     output: {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
-};
+});
