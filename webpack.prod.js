c2856be04ff7dd7b4190b845bf1cf61cda8c6ee1
Zahra <hizahrazadeh@gmail.com>
2021-03-30 18:04:53 -0400

    added property to output in prod config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
     mode: "production",
     output: {
         filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "./imgs/[name].[hash].[ext]",
     },
 });
